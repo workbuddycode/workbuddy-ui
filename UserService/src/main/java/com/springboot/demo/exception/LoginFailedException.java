99bba2aa2406ecc9cd2a610317041a1c8468fcca
Anpurnanand <atiwary72@gmail.com>
2025-08-11 01:54:41 +0530

    segregated UI and backend code under separate repo

--- UserService/src/main/java/com/springboot/demo/exception/LoginFailedException.java
+++
@@ -1,7 +0,0 @@
-package com.springboot.demo.exception;
-
-public class LoginFailedException extends RuntimeException{
-    public LoginFailedException(String message) {
-        super(message);
-    }
-}
